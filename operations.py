9e53fca9a1873cc3765dd564d87afcbc5459c9c4
KelG11 <kel.gresham@yahoo.com>
2023-02-20 19:33:49 +0000

    6th

---
+++ operations.py
@@ -0,0 +1,4 @@
+var = input("Please enter a value: ").upper()
+print(var)
+print(len(var))
+print(var.isdecimal())
---
+++ types_practice.py
@@ -0,0 +1,12 @@
+first_name = "Kelly"
+second_name = "Gresham"
+print(first_name + " " + second_name)
+mylist = [first_name,second_name]
+print(mylist)
+mydict = {"first":first_name,"second":second_name}
+print(mydict)
+
+
+
+
+
